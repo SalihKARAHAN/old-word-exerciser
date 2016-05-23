883b155f228445e67662664a5360d551e79d81de
SalihKARAHAN <salihkarahan@outlook.com>
2016-05-23 17:29:57 +0300

    add method overloads for View(...)

--- src/service/lib/Controller.ts
+++ src/service/lib/Controller.ts
@@ -1,15 +1,23 @@
+import IResult = require('./Result/IResult');
 import ViewResult = require('./Result/ViewResult');
 
 class Controller{
-    
-    View(): ViewResult {
-        let result:ViewResult = new ViewResult();
-        return result;
+
+    public View(viewPath?:string):IResult;
+    public View(controllerName:string, actionName:string):IResult;
+
+    public View(viewPathOrControllerName:string, actionName?:string):IResult{
+            let result:ViewResult = new ViewResult();
+            return result;
     }
 
     Json():any{
 
     }
+
+    //return View
+    //return Json
+    //return Redirect
 }
 
 export = Controller;
